e1ee7583bbf577a27ac43d310450ba966f5a16c3
hugomendonca98 <hugomendonca9@gmail.com>
2022-09-29 11:54:17 -0300

    :wrench: Configurando Styled-components
    Configurando styled-components e adicionando stylos globais.

---
+++ src/styles/Global.ts
@@ -0,0 +1,54 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+
+  :root {
+    --primary-red: #F52D2D;
+    --blue: #3A2FAF;
+    --white: #FFFFFF;
+    --gray: #A8A8A8;
+    --black: #282828;
+    --gradient: linear-gradient(215.46deg, #F52D2D 17.76%, #3A2FAF 85.58%);
+  }
+
+  body {
+    font-family: Helvetica, sans-serif;
+    font-size: 0.75rem;
+    color: var(--white);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  body,
+  input,
+  textarea,
+  select,
+  button {
+    font: 400 1rem Helvetica, sans-serif;
+  }
+
+  // acessibilidade no zom da font;
+  @media (max-width: 1000px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+`;
